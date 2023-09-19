ce58b8bbf48ea5967bf912ed45c24686f2f2b588
ouadala <121585783+ouadala@users.noreply.github.com>
2023-09-19 23:47:06 +0200

    Delete src/main/java/com/example/Exagest/security directory

--- src/main/java/com/example/Exagest/security/exceptions/RoleAlreadyExistException.java
+++
@@ -1,7 +0,0 @@
-package com.example.Exagest.security.exceptions;
-
-public class RoleAlreadyExistException extends Exception{
-    public RoleAlreadyExistException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/example/Exagest/security/utils/constants/JwtConstant.java
+++
@@ -1,7 +0,0 @@
-package com.example.Exagest.security.utils.constants;
-
-public class JwtConstant {
-    public static final long EXPIRATION_TIME = 864000000;
-    public static final String TOKEN_PREFIX = "Bearer ";
-    public static final String HEADER_STRING = "Authorization";
-}
